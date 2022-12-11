32c1f7a910a7a456ae282c3519ea0d1540c2583c
mrspaar <julienpistre.cagnes@gmail.com>
2022-12-11 17:18:46 +0100

    Added boid simulation loop

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include "csdl.h"
+#include "boids.h"
 
 int main() {
     CSDL csdl = CSDL_Init(1024, 720);
 
     SDL_Event event;
-    int count = 20;
+    boid_list boids = create_boid_list(300);
 
     while (1) {
         while(SDL_PollEvent(&event) != 0)
@@ -15,10 +16,12 @@ int main() {
         SDL_RenderClear(csdl.renderer);
         SDL_SetRenderDrawColor(csdl.renderer, 186, 166, 116, 0);
 
-        count++;
-        CSDL_RenderFillCircle(csdl.renderer, count, count, 20);
+        for (int i=0; i<boids.count; i++) {
+            update_boid(boids.list[i], &boids);
+            CSDL_RenderFillCircle(csdl.renderer, boids.list[i]->pos.x, boids.list[i]->pos.y, 2);
+        }
 
         SDL_RenderPresent(csdl.renderer);
-        SDL_Delay(2);
+        SDL_Delay(4);
     }
 }
